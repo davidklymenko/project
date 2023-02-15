b41cb4ff5dc050d2913b20437c4e0a06c0904a2e
David <example@gmail.com>
2023-02-15 15:14:28 +0200

    first commit

---
+++ JS/js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+
+//const question =confirm("Are you here now?");
+//console.log(question);
+
+//const text = + prompt("Did you have 18? If you don't close the web", "");
+//console.log(text + 5);
+
+//const answers = [];
+
+//answers[0] = prompt('Are you Christina Bohonko?', '');
+//answers[1] = prompt('Your Work', '');
+//answers[2] = prompt('Did u have kids?', '');
+
+//document.write(answers);
+
+//let answer = confirm('Are you here now?');
+//console.log(answer);
+
+//let textmind = prompt('Did you have nice day?', "");
+//console.log(textmind);
+
+let firstname = 'Vitalina';
+console.log(firstname);
+
+let numbers = 2.9;
+console.log(-4/0);
+
+const obj = {
+    name: 'Vitalina',
+    age:12,
+    familia: 'Fedyk',
+
+};
+
+const answer = prompt("Are you now?", "");
+console.log(answer);
