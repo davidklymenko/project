4b291ea66f1fcc7d72b9ccb293fce22e2eec43e0
David <example@gmail.com>
2023-02-20 17:01:06 +0200

    js.txt

---
+++ JS/js/script.js
@@ -0,0 +1,38 @@
+"use sctrict"
+
+//let abc = 3.4;
+
+//console.log(-3/0);
+
+const obj = {
+    age:29,
+    name:'Dodo',
+    isMarried: false
+};
+
+console.log(obj.name);
+
+const obb = ['mario.png', 'JS.txt', 2];
+console.log(obb[0]);
+
+let isClosed = false,
+    idOpened = false;
+
+console.log(isClosed || !idOpened);
+
+//const main = + prompt("Are you Baby?", "");
+//console.log(main + 10);
+
+let category = 'toys';
+console.log(`https://howmnany/${category}`);
+
+console.log(10%2);
+
+const answers = [];
+
+answers[0] =  prompt("How many 5+5", "");
+answers[1] =  prompt("How many 2+2", "");
+answers[2] =  prompt("How many 10+5", "");
+answers[3] =  prompt("How many 9+9", "");
+
+document.write(answers)
