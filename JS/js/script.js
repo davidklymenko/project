89e5c8083c41ab81ddcd3797705c7570cbbd4424
David <example@gmail.com>
2023-03-03 20:34:33 +0200

    conditions

--- JS/js/script.js
+++ JS/js/script.js
@@ -1,23 +1,2 @@
-"use sctrict"
+"use strict"
 
-const numberOfFilms = + prompt("Скільки фільмів ви вже переглянули?", "");
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {
-      'logan': '8.1'
-    },
-    actors:{},
-    genres:[],
-    privat: false
-};
-
-const a = prompt('Один із останіх переглянутих фільмів?', ''),
-      b = prompt('Наскільки оцінете його?', '');
-      c = prompt('Один із останіх переглянутих фільмів?', ''),
-      d = prompt('Наскільки оцінете його?', '');
-
-personalMovieDB[a] = b;
-personalMovieDB[c] = d;
-
-console.log(personalMovieDB);
